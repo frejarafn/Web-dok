57b4a64df0fca2ce69ccf06852509a480d5eaed2
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-04-01 09:20:53 +0200

    news

--- javascript.js
+++ javascript.js
@@ -30,12 +30,3 @@ gsap.registerPlugin(ScrollTrigger);
   });
 
 
-//   Forsøg på imagefade
-var nystories = document.querySelector("p").offsetTop;
-window.onscroll = function() {
-  if (window.pageYOffset > 0) {
- var opac = (window.pageYOffset / nystories);
-    console.log(opac);
-  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(images/guld.png) no-repeat";
-  }
-}
